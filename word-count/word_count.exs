62c199a2d8750732b04aa13e3a16c8295c524d00
Tomasz Radziejewski <tomasz@radziejewski.pl>
2017-02-15 23:18:31 +0100

    Word Count

---
+++ word-count/word_count.exs
@@ -0,0 +1,14 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+    sentence
+    |> String.downcase
+    |> String.split(~r/[^[:alnum:]\-]+/u, trim: true)
+    |> Enum.reduce(%{}, fn(word, acc) -> Map.update(acc, word, 1, &(&1 + 1)) end)
+  end
+end
