04f5b52f9000376b3925469339cee3fc629a3174
Tomasz Radziejewski <tomasz@radziejewski.pl>
2017-02-15 23:17:52 +0100

    Nucleotide Count

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,44 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    unless Enum.member?(@nucleotides, nucleotide) do
+      raise ArgumentError, message: "Invalid nucleotide: #{nucleotide}"
+    end
+
+    strand |> Enum.count(&(&1 == nucleotide))
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    unless Enum.all?(
+      strand,
+      fn x -> Enum.member?(@nucleotides, x) end
+    ) do
+      raise ArgumentError, message: "Invalid nucleotide"
+    end
+
+    @nucleotides |> Map.new(&({ &1, count(strand, &1) }))
+  end
+end
